e36e73007a0ab4bb4d6b43e646ec2548fbb79d83
WesleyRo34 <146877895+WesleyRo34@users.noreply.github.com>
2023-10-30 16:41:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+
+        document.getElementById("meuFormulario").addEventListener("submit", function(event) {
+            event.preventDefault(); // Impede o envio padrão do formulário
+            const nome = document.getElementById("nome").value;
+            const mensagem = document.getElementById("mensagem").value;
+            alert("Nome: " + nome + "\nMensagem: " + mensagem);
+        });
+
